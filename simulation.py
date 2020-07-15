cd6ea85985dde14d8672f02eb32e2df07df7a7d0
Damango <37273048+Damango@users.noreply.github.com>
2020-07-15 19:52:43 -0400

    First file upload :)

---
+++ simulation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 17:17:48 2020
+
+@author: justinkessler
+"""
+import turtle
+
+
+width = 500
+height = 500
+
+
+window = turtle.Screen()
+window.setup(width,height)
+while True:
+    window.update()
+window.mainloop()
